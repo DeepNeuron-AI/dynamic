4a7d62cc922b4c56202a910de0d229ccb7fc8585
Lex Gallon <lexlmgallon@gmail.com>
2023-01-22 23:01:24 +1100

    Remove click from echonet-dynamic code
    - Update requirements.txt. No longer specifies any versions.
    - Add environment.yml which seems to build environment in full without
      error! Includes the requirements.txt. Pretty much everything installed
      via pip because it just plays nicer with opencv, etc.
    - Replace any references to np.int with np.int8 since np.int no longer
      supported in numpy (see https://levelup.gitconnected.com/fix-attributeerror-module-numpy-has-no-attribute-float-d7d68c5a4971)!
    - Remove references to click form echonet-dynamic code. Means imports
      will have to change in future, and no more nice command-line interface
      but it works for what I'm doing!
    - Add venv and .vscode to .gitignore.

--- echonet/utils/segmentation.py
+++ echonet/utils/segmentation.py
@@ -4,7 +4,7 @@ import math
 import os
 import time
 
-import click
+# import click
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal
@@ -16,26 +16,26 @@ import tqdm
 import echonet
 
 
-@click.command("segmentation")
-@click.option("--data_dir", type=click.Path(exists=True, file_okay=False), default=None)
-@click.option("--output", type=click.Path(file_okay=False), default=None)
-@click.option("--model_name", type=click.Choice(
-    sorted(name for name in torchvision.models.segmentation.__dict__
-           if name.islower() and not name.startswith("__") and callable(torchvision.models.segmentation.__dict__[name]))),
-    default="deeplabv3_resnet50")
-@click.option("--pretrained/--random", default=False)
-@click.option("--weights", type=click.Path(exists=True, dir_okay=False), default=None)
-@click.option("--run_test/--skip_test", default=False)
-@click.option("--save_video/--skip_video", default=False)
-@click.option("--num_epochs", type=int, default=50)
-@click.option("--lr", type=float, default=1e-5)
-@click.option("--weight_decay", type=float, default=0)
-@click.option("--lr_step_period", type=int, default=None)
-@click.option("--num_train_patients", type=int, default=None)
-@click.option("--num_workers", type=int, default=4)
-@click.option("--batch_size", type=int, default=20)
-@click.option("--device", type=str, default=None)
-@click.option("--seed", type=int, default=0)
+# @click.command("segmentation")
+# @click.option("--data_dir", type=click.Path(exists=True, file_okay=False), default=None)
+# @click.option("--output", type=click.Path(file_okay=False), default=None)
+# @click.option("--model_name", type=click.Choice(
+#     sorted(name for name in torchvision.models.segmentation.__dict__
+#            if name.islower() and not name.startswith("__") and callable(torchvision.models.segmentation.__dict__[name]))),
+#     default="deeplabv3_resnet50")
+# @click.option("--pretrained/--random", default=False)
+# @click.option("--weights", type=click.Path(exists=True, dir_okay=False), default=None)
+# @click.option("--run_test/--skip_test", default=False)
+# @click.option("--save_video/--skip_video", default=False)
+# @click.option("--num_epochs", type=int, default=50)
+# @click.option("--lr", type=float, default=1e-5)
+# @click.option("--weight_decay", type=float, default=0)
+# @click.option("--lr_step_period", type=int, default=None)
+# @click.option("--num_train_patients", type=int, default=None)
+# @click.option("--num_workers", type=int, default=4)
+# @click.option("--batch_size", type=int, default=20)
+# @click.option("--device", type=str, default=None)
+# @click.option("--seed", type=int, default=0)
 def run(
     data_dir=None,
     output=None,
