e088bc07d2c2c48879f7e66537d138ef749551d1
Allisterlim <Allisterlim77@gmail.com>
2023-04-30 11:38:43 +1000

    Added MedianBlur
    Branch for Denoising
    
    Co-authored-by: Lex Gallon <byebrid@users.noreply.github.com>
    Co-authored-by: Colala65 <Colala65@users.noreply.github.com>

--- denoising_training.py
+++ denoising_training.py
@@ -13,48 +13,50 @@ def to_tensor(array: np.ndarray):
         
 def to_float(tensor: torch.Tensor):
     return tensor.float()
-    
+
 
 class DenoisedDataset(Dataset):
 
-    def __init__(self, directory: str, noise_factor: float = 0.1):
+    def __init__(self, directory: str, noise_factor: float = 0.05, kernel_size: int = 11):
         self.directory = Path(directory)
         self.video_directory = self.directory / "Videos"
         self.noise_factor = noise_factor
         
         # Compute mean and std
-        mean, std = echonet.utils.get_mean_and_std(echonet.datasets.Echo(root=str(self.directory), split="train"))
-        self.mean = mean 
-        self.std = std 
+        # mean, std = echonet.utils.get_mean_and_std(echonet.datasets.Echo(root=str(self.directory), split="train"))
+        # print(mean, std)
+        self.mean = 32.823921 # [~50, ~50, ~50]
+        self.std = 50.203746  # [~70, ~70, ~70]
 
         # Get filepaths for each video
         self.filepaths = list(self.video_directory.iterdir())
 
-
         self.transform = transforms.Compose([
-            Transpose((1, 2, 3, 0)), # (frame, height, width, color)
-            Normalize(mean, std),
-            Transpose((0, 3, 1, 2)), # (frames, color, height, width)
+            ConvertColor(), # (frame, height, width)
+            Normalize(self.mean, self.std),
             to_tensor
-            # transpose_video,
-            # to_tensor,
-            # to_float, 
-            # normalize,
-            # transforms.Normalize(mean, std),
         ])
-        self.noise = AddGaussianNoise(0., noise_factor)
-
-        print(self.mean)
-        print(self.std)
+        self.noisy_transform = transforms.Compose([ # (color, frame, height, width)
+            Transpose((1, 2, 3, 0)), # (frame,height, width, color)
+            MedianBlur(kernel_size=kernel_size), # (frame,height, width, color)
+            Transpose((3, 0, 1, 2)), # (color, frame, height, width)
+            ConvertColor(), # (frame, height, width)
+            Normalize(self.mean, self.std),
+            to_tensor,
+            AddGaussianNoise(std=noise_factor)
+        ])
 
     def __getitem__(self, index):
-        video = echonet.utils.loadvideo(str(self.filepaths[index]))
-        video = self.transform(video)
-        noisy_video = self.noise(video)
+        raw_video = echonet.utils.loadvideo(str(self.filepaths[index])) # (color, frame, height, width)
+        video = self.transform(raw_video)
+        noisy_video = self.noisy_transform(raw_video)
+        # video = to_tensor(video)
+        # noisy_video = to_tensor(noisy_video)
         return noisy_video, video
 
     def __len__(self):
         return len(self.filepaths) 
+
 
 class AddGaussianNoise(object):
     '''
@@ -79,6 +81,17 @@ class Transpose:
         return array.transpose(self.dims)      
 
 
+class ConvertColor:
+    def __call__(self, array: np.ndarray):
+        """
+        Parameters
+        ---------
+        array: np.ndarray
+            Dimensions should be (color, frame, height, width)
+        """
+        return (0.299*array[2] + 0.587*array[1] + 0.114*array[0]).astype(np.uint8)
+
+
 class Normalize:
     def __init__(self, mean, std) -> None:
         self.mean = mean
@@ -88,21 +101,35 @@ class Normalize:
         return (array - self.mean) / self.std
 
 
+class MedianBlur:
+    def __init__(self, kernel_size: int) -> None:
+        if kernel_size % 2 == 0:
+            raise ValueError(f"Kernel size should be odd integer but was {kernel_size}!")
+
+        self.kernel_size = kernel_size
+
+    def __call__(self, array: np.ndarray) -> np.ndarray:
+        result = np.zeros(array.shape, dtype=array.dtype)
+        for i, frame in enumerate(array):
+            print(i)
+            result[i] = cv2.medianBlur(frame, self.kernel_size)
+        return result
+        
+
+
 
 if __name__ == "__main__":
     directory = r"C:\Users\Allis\Documents\MDN\Ultrasound2023\dynamic\a4c-video-dir"
-    dataset = DenoisedDataset(directory, noise_factor=1.)
-    noisy_video, video = dataset[100]
+    dataset = DenoisedDataset(directory, noise_factor=0.1)
+    noisy_video, video = dataset[1000]
     noisy_video = noisy_video.numpy()
     video = video.numpy()
 
     mean, std = dataset.mean, dataset.std
-    video = video.transpose((0, 2, 3, 1)) # (frame, height, width, color)
     print(((video * std + mean) < 0).sum())
     video = (video * std + mean).astype(np.uint8)
     # video = video.transpose((0, 3, 1, 2)) 
 
-    noisy_video = noisy_video.transpose((0, 2, 3, 1)) # (frame, height, width, color)
     print(((noisy_video * std + mean) < 0).sum())
     noisy_video = noisy_video * std + mean
     noisy_video = np.clip(noisy_video, 0, 255)
